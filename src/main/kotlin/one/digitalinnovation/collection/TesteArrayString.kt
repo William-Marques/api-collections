e40f54ccf87be486b60f1e9ef92558d0aa28dd17
William Marques <matrix.consultoria@gmail.com>
2021-08-27 18:28:59 -0300

    created int array test class

---
+++ src/main/kotlin/one/digitalinnovation/collection/TesteArrayString.kt
@@ -0,0 +1,23 @@
+package one.digitalinnovation.collection
+
+fun main() {
+    val nomes = Array<String>(3){""}
+    nomes[0] = "Maria"
+    nomes[1] = "Zazá"
+    nomes[2] = "José"
+
+    for (nome in nomes) {
+        println(nome)
+    }
+
+    println("__________________")
+    nomes.sort()
+    nomes.forEach {
+        println(it)
+    }
+
+    println("__________________")
+    val nomes2 = arrayOf("Maria", "Zazá", "Pedro")
+    nomes2.sort()
+    nomes2.forEach { println(it) }
+}
